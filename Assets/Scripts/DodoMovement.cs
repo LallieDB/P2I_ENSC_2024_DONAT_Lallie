f33a4b63d0d87dc695e172f151541f5bcd80bdcb
Donat-Bouillud Lallie <ldonatbouil@ensc.fr>
2024-01-20 11:41:25 +0100

    Ajout des transitions pour le deplacement du dodo

--- Assets/Scripts/DodoMovement.cs
+++ Assets/Scripts/DodoMovement.cs
@@ -5,8 +5,13 @@ public class dodoMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed;
+    // public float jumpForce;
     public Rigidbody2D body;
+    public Animator animator;
     private Vector3 velocity = Vector3.zero;
+
+    // private bool isJumping;
+
     void Start()
     {
         
@@ -18,12 +23,28 @@ public class dodoMovement : MonoBehaviour
         //At each frame, we reset the horizontal and vertical movements of the dodo
         float horizontalMovement = Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime;
         float verticalMovement = Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;
-        MoveDodo(horizontalMovement, verticalMovement);
+        // if(Input.GetButtonDown("Jump"))
+        // {
+        //     isJumping=true;
+        // }
 
+        MoveDodo(horizontalMovement, verticalMovement);
+        SetAnimator();
     }
     void MoveDodo(float _horizontalMovement, float _verticalMovement)
     {
         Vector3 targetVelocity =new Vector2(_horizontalMovement, _verticalMovement);
         body.velocity=Vector3.SmoothDamp(body.velocity, targetVelocity, ref velocity, .05f);
+        // if (isJumping==true)
+        // {
+        //     body.AddForce(new Vector2(0f,jumpForce));
+        //     isJumping=false;
+        // }
+    }
+    void SetAnimator()
+    {
+        animator.SetFloat("HorizontalSpeed",body.velocity.x);
+        animator.SetFloat("VerticalSpeed",body.velocity.y);
+
     }
 }
