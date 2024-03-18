44cf2a89bf8746fb6404bc30d667f7d36263cda1
Lallie Donat <ldonatbouil@ensc.fr>
2024-03-18 15:36:48 +0100

    New dialogues for the parrot plant : first the plant ask the dodo to search for water, if he accepts then there is a new dialogue and when the dodo says that he has water, the plant disappear

--- Assets/Scripts/PlantScript.cs
+++ Assets/Scripts/PlantScript.cs
@@ -1,22 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlantScript : MonoBehaviour
 {
     private GameObject interactionDialogue;
+    private GameObject questIcon;
+    private GameObject plant;
     private Dialogue plantDialogue;
     public Rigidbody2D dodoBody;
     public Rigidbody2D plantBody;
     public bool isInRange;
+    public bool acceptQuest;
     // Start is called before the first frame update
     void Start()
-    {
+    {   //assignation of the GameObject
+        questIcon=GameObject.Find("quest_icon");
         interactionDialogue=GameObject.Find("DialogueInteraction");
+        plant=GameObject.Find("Plant");
+        //Assignation of beginning dialogue and booleens
         plantDialogue=CreatePlantDialogue();
         isInRange=false;
+        acceptQuest=false;
     }
 
     // Update is called once per frame
@@ -26,6 +34,20 @@ public class PlantScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P)) //if the dodo is in range and the player press P, the dialogue begins
         {
             TriggerDialogue();
+        }
+        //if the player accept the quest, the dialogue changes
+        if (acceptQuest==false && plantDialogue.lines[1].choice1.isChosen==true)
+        {
+            acceptQuest=true;
+            plantDialogue=PlantWaitingWaterDialogue();
+            questIcon.SetActive(false);
+        }
+        //if the player succeed at the quest, the plant disappear
+        else if( acceptQuest==true && plantDialogue.lines[1].choice1.isChosen==true)
+        {
+            Destroy(plant);
+            Destroy(plantBody);
+            interactionDialogue.GetComponentInChildren<Text>().text="";
         }
         
     }
@@ -50,6 +72,7 @@ public class PlantScript : MonoBehaviour
     { // if the player is close to the parrot and press P, the dialogue is trigger
         if ( IsInRangeDialogue()==true)
         {
+            //if the quest is accepted, we change the dialogue
             DialogueManager.instance.EndDialogue(); // close others dialogues
             DialogueManager.instance.ChangingDialogue(plantDialogue); //use the parrot dialogue
             DialogueManager.instance.StartDialogue(); // start dialogue
@@ -58,14 +81,28 @@ public class PlantScript : MonoBehaviour
     
     
     private Dialogue CreatePlantDialogue()
-    {
+    { //Creation of the first dialogue of the parrot plant
         Dialogue dialogue = new Dialogue();
         dialogue.name = "Plant";
         dialogue.lines = new DialogueLine[]
         {
             new DialogueLine("Hello there !",false),
-            new DialogueLine("You seem to be a nice bird. Please bring me water, my leafs start to deplenished",false),
+            new DialogueLine("You seem to be a nice bird. Please bring me water, my leafs start to deplenished",true, 
+            new Choice("Yes", new DialogueLine("Thank you, you are a good bird.", false)), new Choice("No", new DialogueLine("Pleease, I am begging you", false))),
         };
         return dialogue;
+    }
+    private Dialogue PlantWaitingWaterDialogue()
+    { //Creation of the dialogue when the player accept the quest of the parrot plant
+        Dialogue dialogue = new Dialogue();
+        dialogue.name = "Plant";
+        dialogue.lines = new DialogueLine[]
+        {
+            new DialogueLine("Hello again nice bird !",false),
+            new DialogueLine("Do you have water ?",true, 
+            new Choice("Yes", new DialogueLine("Thank you so much ! Oh, but why me leafs deplenished so fast ?!?!?!?!", false)), new Choice("No", new DialogueLine("Hurry !", false))),
+        };
+        return dialogue;
+
     }
 }
